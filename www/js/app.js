318cb8df0b16093d084dfe85d9dd6e982667b1ef
Vladimir Tonkonogov <tonk0006@algonquinlive.com>
2015-04-07 00:38:00 -0400

    Basic navigation done

---
+++ www/js/app.js
@@ -0,0 +1,218 @@
+//tonk0006_giftr.js
+
+var tonk0006_giftr = {
+    loadRequirements: 0,
+    init: function () {
+        document.addEventListener('deviceready', this.onDeviceReady);
+        document.addEventListener('DOMContentLoaded', this.onDomReady);
+    },
+    onDeviceReady: function () {
+        //alert("Device is ready");
+//        tonk0006_giftr.loadRequirements++;
+//        if (tonk0006_giftr.loadRequirements === 2) {
+            tonk0006_giftr.start();
+//        }
+    },
+    onDomReady: function () {
+        //alert("DOM is ready");
+//        tonk0006_giftr.loadRequirements++;
+//        if (tonk0006_giftr.loadRequirements === 2) {
+            tonk0006_giftr.start();
+//        }
+    },
+    start: function () {
+        //connect to database
+        //build the lists for the main pages based on data
+        //add button and navigation listeners
+
+        tonk0006_giftr.displayPeoplePage();
+                
+        var addButtons = document.querySelectorAll('.btnAdd');
+        addButtons[0].addEventListener('click', tonk0006_giftr.openNewPersonModal);
+        addButtons[1].addEventListener('click', tonk0006_giftr.openNewOccasionModal);
+        addButtons[2].addEventListener('click', tonk0006_giftr.openNewGiftForPersonModal);
+        addButtons[3].addEventListener('click', tonk0006_giftr.openNewGiftForOccasionModal);
+
+        var cancelButtons = document.querySelectorAll('.btnCancel');
+        cancelButtons[0].addEventListener('click', tonk0006_giftr.displayPeoplePage);
+        cancelButtons[1].addEventListener('click', tonk0006_giftr.displayOccasionsPage);
+        cancelButtons[2].addEventListener('click', tonk0006_giftr.displayGiftsForPeoplePage);
+        cancelButtons[3].addEventListener('click', tonk0006_giftr.displayGiftsForOccasionsPage);
+        
+        var saveButtons = document.querySelectorAll('.btnSave');
+        saveButtons[0].addEventListener('click', tonk0006_giftr.savePerson);
+        saveButtons[1].addEventListener('click', tonk0006_giftr.saveOccasion);
+        saveButtons[2].addEventListener('click', tonk0006_giftr.saveItem);
+        saveButtons[3].addEventListener('click', tonk0006_giftr.saveItem);
+        
+        document.querySelector('header').addEventListener('click', tonk0006_giftr.displayPeoplePage);
+                
+        var list = document.querySelectorAll('[data-role="listview"]');
+        var hm1 = new Hammer.Manager(list[0]);
+        var hm2 = new Hammer.Manager(list[1]);
+
+        var singleTap = new Hammer.Tap({
+            event: 'singletap'
+        });
+        
+        var doubleTap = new Hammer.Tap({
+            event: 'doubletap',
+            taps: 2,
+            threshold: 10,
+            posThreshold: 40
+        });
+        
+        hm1.add([singleTap, doubleTap]);
+        doubleTap.requireFailure(singleTap);
+        
+        hm1.on('singletap', tonk0006_giftr.displayGiftsForPeoplePage);
+        hm2.on('singletap', tonk0006_giftr.displayGiftsForOccasionsPage);
+        hm1.on('doubletap', tonk0006_giftr.deleteListItem);
+        hm2.on('doubletap', tonk0006_giftr.deleteListItem);
+//
+//        var swipeLeft = new Hammer.Tap({
+//            event: 'swipeleft'
+//        });
+//        var swipeRight = new Hammer.Tap({
+//            event: 'swiperight'
+//        });
+        
+        var goToPeople = new Hammer(document.querySelector('[data-role=page]#occasion-list'));
+        goToPeople.on('swiperight', tonk0006_giftr.displayPeoplePage);
+        
+        var goToOccassions = new Hammer(document.querySelector('[data-role=page]#people-list'));
+        goToOccassions.on('swipeleft', tonk0006_giftr.displayOccasionsPage);
+
+        
+        
+        
+        
+        
+
+//        var newPersonModal = new Hammer(document.querySelector('#people-list .btnAdd'));
+//        newPersonModal.on('singletap', tonk0006_giftr.openNewPersonModal);
+//
+//        var newOccasionnModal = new Hammer(document.querySelector('#occasion-list .btnAdd'));
+//        newOccasionnModal.on('singletap', tonk0006_giftr.openNewOccasionModal);
+//        
+//        var cancelPersonModal = new Hammer(document.querySelector('#add-person .btnCancel'));
+//        cancelPersonModal.on('singletap', tonk0006_giftr.displayPeoplePage);
+//        
+//        var cancelOccasionModal = new Hammer(document.querySelector('#add-occasion .btnCancel'));
+//        cancelOccasionModal.on('singletap', tonk0006_giftr.displayOccasionsPage);
+    },
+
+    displayPeoplePage: function () {
+        document.querySelector('[data-role=modal]#add-person').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+        document.querySelector('[data-role=page]#people-list').style.display = 'block';
+        document.querySelector('[data-role=page]#occasion-list').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-person').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-occasion').style.display = 'none';
+    },
+
+    displayOccasionsPage: function () {
+        document.querySelector('[data-role=modal]#add-occasion').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+        document.querySelector('[data-role=page]#people-list').style.display = 'none';
+        document.querySelector('[data-role=page]#occasion-list').style.display = 'block';
+        document.querySelector('[data-role=page]#gifts-for-person').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-occasion').style.display = 'none';
+    },
+    
+    displayGiftsForPeoplePage: function () {
+        document.querySelector('[data-role=modal]#add-gift-per').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+        document.querySelector('[data-role=page]#people-list').style.display = 'none';
+        document.querySelector('[data-role=page]#occasion-list').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-person').style.display = 'block';
+        document.querySelector('[data-role=page]#gifts-for-occasion').style.display = 'none';
+    },
+    
+    displayGiftsForOccasionsPage: function () {
+        document.querySelector('[data-role=modal]#add-gift-occ').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+        document.querySelector('[data-role=page]#people-list').style.display = 'none';
+        document.querySelector('[data-role=page]#occasion-list').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-person').style.display = 'none';
+        document.querySelector('[data-role=page]#gifts-for-occasion').style.display = 'block';
+    },
+
+    openNewPersonModal: function () {
+        console.log('Person modal window opened');
+        document.querySelector('[data-role=modal]#add-person').style.display = 'block';
+        document.querySelector('[data-role=overlay]').style.display = 'block';
+        //document.querySelector('#new-per').value = '';
+        var input = document.querySelector('#new-per');
+        input.value = '';
+        input.focus();
+        input.addEventListener('keypress', function (ev) {
+            ev.stopImmediatePropagation();
+            if (ev.keyCode === 13) {
+                ev.preventDefault();
+                //console.log(ev);
+                tonk0006_giftr.savePerson();
+                input.blur();
+            }
+        });
+    },
+
+    openNewOccasionModal: function () {
+        console.log('Occasion modal window opened');
+        document.querySelector('[data-role=modal]#add-occasion').style.display = 'block';
+        document.querySelector('[data-role=overlay]').style.display = 'block';
+        var input = document.querySelector('#new-occ');
+        input.value = '';
+        input.focus();
+        input.addEventListener('keypress', function (ev) {
+            ev.stopImmediatePropagation();
+            if (ev.keyCode === 13) {
+                ev.preventDefault();
+                tonk0006_giftr.saveOccasion();
+                input.blur();
+            }
+        });
+        
+    },
+    
+    openNewGiftForPersonModal: function () {
+        console.log('Gift for person modal window opened');
+        document.querySelector('[data-role=modal]#add-gift-per').style.display = 'block';
+        document.querySelector('[data-role=overlay]').style.display = 'block';
+    },
+    
+    openNewGiftForOccasionModal: function () {
+        console.log('Gift for occasion modal window opened');
+        document.querySelector('[data-role=modal]#add-gift-occ').style.display = 'block';
+        document.querySelector('[data-role=overlay]').style.display = 'block';
+    },
+    savePerson: function () {
+        var ul = document.querySelectorAll('[data-role="listview"]');
+        var li = document.createElement('li');
+        var text = document.querySelector('#new-per').value;
+        li.innerHTML = text;
+        if (text)
+            ul[0].appendChild(li);
+        document.querySelector('[data-role=modal]#add-person').style.display = 'none';
+        document.querySelector('[data-role=modal]#add-occasion').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+    },
+    
+    saveOccasion: function () {
+        var ul = document.querySelectorAll('[data-role="listview"]');
+        var li = document.createElement('li');
+        var text = document.querySelector('#new-occ').value;
+        li.innerHTML = text;
+        if(text)
+            ul[1].appendChild(li);
+        document.querySelector('[data-role=modal]#add-person').style.display = 'none';
+        document.querySelector('[data-role=modal]#add-occasion').style.display = 'none';
+        document.querySelector('[data-role=overlay]').style.display = 'none';
+    }
+}
+
+tonk0006_giftr.init();
+
+//function test(){
+//    alert("TEST");
+//}
---
+++ www/js/sample.js
@@ -0,0 +1,215 @@
+//app.js
+
+var app= {
+    db:null,
+	version:'1.0',
+	loadRequirements:0,
+	init: function(){
+		//document.addEventListener("deviceready", app.onDeviceReady);
+		document.addEventListener("DOMContentLoaded", app.onDomReady);
+	},
+	onDeviceReady: function(){
+		//app.loadRequirements++;
+		//if(app.loadRequirements === 2){
+			//app.start();
+		//}
+	},
+	onDomReady: function(){
+		//app.loadRequirements++;
+		//if(app.loadRequirements === 2){
+			app.start();
+		//}
+	},
+	start: function(){
+		app.createdb();
+		app.fillpeople();
+		//app.filloccasions();
+	
+		//connect to database
+		//build the lists for the main pages based on data
+		//add button and navigation listeners
+	},
+	createdb: function(){
+		tonk0006_giftr.db = openDatabase('giftr','','Giftr',1024*1024);
+		
+		if(app.version == ''){
+			console.log('First time running... create tables'); 
+			//means first time creation of DB
+			//increment the version and create the tables
+			tonk0006_giftr.db.changeVersion('', app.version,
+					function(trans){
+						//something to do in addition to incrementing the value
+						//otherwise your new version will be an empty DB
+						console.log("DB version incremented");
+						//do the initial setup
+								//create some table(s)
+								//add stuff into table(s)
+						trans.executeSql('CREATE TABLE people(person_id INTEGER PRIMARY KEY AUTOINCREMENT, person_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table people created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO people(person_name) VALUES(?)', ["Vlad"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											console.log("Added row in people");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+						trans.executeSql('CREATE TABLE occasions(occ_id INTEGER PRIMARY KEY AUTOINCREMENT, occ_name TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table occ created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+										
+						trans.executeSql('INSERT INTO occasions(occ_name) VALUES(?)', ["Birthday"], 
+										function(tx, rs){
+											//do something if it works, as desired   
+											console.log("Added row in occ");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+						
+						trans.executeSql('CREATE TABLE gifts(gift_id INTEGER PRIMARY KEY AUTOINCREMENT, person_id INTEGER, occ_id INTEGER, gift_idea TEXT, purchased TEXT)', [], 
+										function(tx, rs){
+											//do something if it works
+											console.log("Table gift created");
+										},
+										function(tx, err){
+											//failed to run query
+											console.log( err.message);
+										});
+					},
+					function(err){
+						//error in changing version
+						//if the increment fails
+						console.log( "Change version call error " + err.message);
+					},
+					function(){
+						//successfully completed the transaction of incrementing the version number   
+								tonk0006_giftr.version = '1.0';
+								console.log("Change version function worked.");
+					});
+		}else{
+			//version should be 1.0
+			//this won't be the first time running the app
+			console.log("DB has previously been created");
+			console.log('Version:' + tonk0006_giftr.version)   ;
+		}
+	},
+	fillpeople: function(){
+        document.querySelector("#people-list").style.display="block";
+        document.querySelector("#occasion-list").style.display="none";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        var ul = document.querySelectorAll('[data-role=listview]')[0];
+        ul.innerHTML = "";
+        
+        app.db.transaction(function(trans){
+        trans.executeSql("SELECT person_name FROM people", [],
+                function(tx, rs){
+                         var len = rs.rows.length;
+
+                         for (var i=0; i<len; i++) {
+                         // display one person_name
+							 var li = document.createElement("li");
+							 li.dataset.ref = i;
+							 li.innerHTML = rs.rows.item(i).person_name;
+							 ul.appendChild(li);
+                         }
+                },
+                function(tx, err){
+                    console.log("Error: " + err);
+                });
+            });		
+		
+			var hammertime = new Hammer.Manager(ul);	
+			var swipeRight = new Hammer.Tap({event: 'swiperight' });
+			var singleTap = new Hammer.Tap({ event: 'singletap' });
+			var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2});
+			hammertime.add([doubleTap, singleTap, swipeRight]);
+			doubleTap.requireFailure('singletap');
+
+			hammertime.on('swiperight', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				app.filloccasions(ev);
+			});
+
+			hammertime.on('singletap', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				//app.view(ev);
+			});
+			hammertime.on('doubletap', function(ev) {
+				ev.preventDefault();
+				console.log(ev);
+				//app.map(ev);
+			});
+	},
+	filloccasions: function(){
+        document.querySelector("#people-list").style.display="none";
+        document.querySelector("#occasion-list").style.display="block";
+        document.querySelector("#gifts-for-person").style.display="none";
+        document.querySelector("#gifts-for-occasion").style.display="none";
+        
+        var ul = document.querySelectorAll('[data-role=listview]')[1];
+        ul.innerHTML = "";
+        
+        app.db.transaction(function(trans){
+        trans.executeSql("SELECT occ_name FROM occasions", [],
+                function(tx, rs){
+                         var len = rs.rows.length;
+
+                         for (var i=0; i<len; i++) {
+                         // display one person_name
+							 var li = document.createElement("li");
+							 li.dataset.ref = i;
+							 li.innerHTML = rs.rows.item(i).occ_name;
+							 ul.appendChild(li);
+                         }
+                },
+                function(tx, err){
+                    console.log("Error: " + err);
+                });
+        });
+		var hammertime = new Hammer.Manager(ul);	
+		var swipeLeft = new Hammer.Tap({event: 'swipeleft' });
+		var singleTap = new Hammer.Tap({ event: 'singletap' });
+		var doubleTap = new Hammer.Tap({event: 'doubletap', taps: 2});
+		hammertime.add([doubleTap, singleTap, swipeRight]);
+		doubleTap.requireFailure('singletap');
+
+		hammertime.on('swipeleft', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			app.fillpeople(ev);
+		});
+
+		hammertime.on('singletap', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			//app.view(ev);
+		});
+		hammertime.on('doubletap', function(ev) {
+			ev.preventDefault();
+			console.log(ev);
+			//app.map(ev);
+		});
+	}
+}
+app.init();
